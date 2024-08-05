2ac282bd7309ae4f750658c780901240297de640
Priyanshu <153304853+priyanshu3679@users.noreply.github.com>
2024-08-06 01:40:51 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -12,18 +12,18 @@ bisal_grp = "https://t.me/+PA8OPL2Zglk3MDM1"
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '26510726'))
-    API_HASH = str(getenv('API_HASH', '3d443d3307f5ef8f0c5a6866b2f65c0e'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN' , '7437401613:AAGD5Jt3vkZ8Og3AqyZdcxWhqK5lL1qPTVI'))
+    API_ID = int(getenv('API_ID', ''))
+    API_HASH = str(getenv('API_HASH', ''))
+    BOT_TOKEN = str(getenv('BOT_TOKEN' , ''))
     name = str(getenv('name', 'filestreambot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002246422784'))
-    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002165368952'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', ''))
+    NEW_USER_LOG = int(getenv('NEW_USER_LOG', ''))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "6994148328").split()]
+    OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "").split()]
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'DevilQueen777'))
@@ -39,7 +39,7 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://pk6937737:pk6937737@cluster0.6chggvt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
+    DATABASE_URL = str(getenv('DATABASE_URL', ''))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', '@mypublicchannel456')) 
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))   
     BAN_CHNL = list(set(int(x) for x in str(getenv("BAN_CHNL", "")).split()))   
